5644f5543d32a1784edb445cce9c951d306a60ba
Mykel Alvis <mykel.alvis@gmail.com>
2023-10-14 10:52:47 -0500

    compiles, at least

--- ibversions-maven-plugin/src/main/java/org/infrastructurebuilder/maven/AbstractGenerateMojo.java
+++ ibversions-maven-plugin/src/main/java/org/infrastructurebuilder/maven/AbstractGenerateMojo.java
@@ -19,14 +19,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
-import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.shared.filtering.MavenResourcesFiltering;
